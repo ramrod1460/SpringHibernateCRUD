5e53636d8d794a188bf3fa937851b39b8d8372a8
Rod Madden <ramrod1460@hotmail.com>
2020-04-06 09:12:28 -0400

    Initial commit

---
+++ src/main/java/com/springbootjpa/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.springbootjpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springbootjpa.model.EmployeeEntity;
+ 
+@Repository
+public interface EmployeeRepository
+        extends JpaRepository<EmployeeEntity, Long> {
+ 
+}
